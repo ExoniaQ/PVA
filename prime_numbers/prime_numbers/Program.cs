02fc6b3ea1b61505954fc9de264487741dd315d6
ExoniaQ <spect8494@gmail.com>
2022-10-19 11:37:37 +0200

    prime

---
+++ prime_numbers/prime_numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace prime_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong num = Convert.ToUInt64(Console.ReadLine());
+            bool prime = true;
+
+            if (num % 2 == 0 && num > 2)
+            {
+                prime = false;
+            }
+            else
+            {
+                int boundary = (int)Math.Sqrt(num);
+                for (int i = 3; i < boundary; i += 2)
+                {
+                    if (num % Convert.ToUInt64(i) == 0)
+                    {
+                        prime = false;
+                    }
+                }
+            }
+            if (prime)
+            {
+                Console.WriteLine("is prime");
+            }
+            else
+            {
+                Console.WriteLine("is not prime");
+            }
+            Console.ReadKey();
+        }
+    }
+}
