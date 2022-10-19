8d7283065170ce1cbe403c860d93d153c4f05bcc
ExoniaQ <spect8494@gmail.com>
2022-10-19 11:31:53 +0200

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            
+            
+            {            
+                /* sort */
+                if (b > c)
+                {
+                    int temp = b;
+                    b = c;
+                    c = temp;
+                }
+                if (a > b)
+                {
+                    int temp = b;
+                    b = a;
+                    a = temp;
+                }
+                if (b > c)
+                {
+                    int temp = b;
+                    b = c;
+                    c = temp;
+                }
+                bool pravost = false;
+                if (c*c == b*b + a*a)
+                {
+                    pravost = true;
+                }
+                
+
+                if (pravost == true)
+                {
+                    Console.WriteLine("trojuhelnik je pravouhly");
+                }
+                else
+                {
+                    Console.WriteLine("trojuhelnik neni pravouhly");
+                }
+
+            }
+            
+        }
+    }
+}
